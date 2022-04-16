e524af00e5bd0007c28ae7ecd8426d52ea28cc4e
Omar-Anzures <omarsatiiago@gmail.com>
2022-04-16 16:07:13 -0500

    actualizado

---
+++ ejemplos-basicos/Funciones/funcion11.py
@@ -0,0 +1,15 @@
+
+
+def segundo(segundos):
+    hora  = segundos / 3600
+    minutos = segundos / 60
+    return hora,minutos
+
+print(segundo(100))
+    
+#def hora(horas):
+    
+    
+    
+    
+    
